6f4aee85f7e8712d3ac3eae6ef3f9c174bdf37f0
Sergio <sdamasoc@hotmail.com>
2023-04-17 21:51:19 +0200

    Update API version

--- plugin.py
+++ plugin.py
@@ -34,7 +34,7 @@
                 <option label="False" value="Normal"  default="true" />
             </options>
         </param>
-        <param field="Mode3" label="API Version" width="60px" required="true" default="1.17.0"/>
+        <param field="Mode3" label="API Version" width="60px" required="true" default="1.18.0"/>
     </params>
 </plugin>
 """
